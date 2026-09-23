cdcd5e54c646896a2aafd88b98076a36dc7547c5
andrebsiqueira <andreb.siqueira@gmail.com>
2026-09-23 17:29:02 -0300

    Set up Google API configuration in chatbot.py
    Configured the Google API using the stored API key.

--- views/chatbot.py
+++ views/chatbot.py
@@ -8,6 +8,9 @@ from google import genai
 # Acessa a chave guardada nos segredos
 google_key = st.secrets["GOOGLE_API_KEY"]
 
+# Configura a API do Google
+genai.configure(api_key=google_key)
+
 #client = genai.Client(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def show_chatbot():
