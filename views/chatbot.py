8b8aa5862f834bf8519468da39d0aa4c6d7e7749
andrebsiqueira <andreb.siqueira@gmail.com>
2026-09-23 20:30:13 -0300

    Change Google API key access method
    Updated the method of accessing the Google API key from Streamlit secrets to environment variables.

--- views/chatbot.py
+++ views/chatbot.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+import os
+
 from banco import conectar
 
 from google import genai
@@ -13,7 +15,8 @@ def show_chatbot():
     )
 
     # Acessa a chave guardada nos segredos
-    google_key = st.secrets["GOOGLE_API_KEY"]
+    # google_key = st.secrets["GOOGLE_API_KEY"]
+    google_key = os.getenv["GOOGLE_API_KEY"]
     
     # Configura a API do Google
     client = genai.Client(api_key=google_key)
