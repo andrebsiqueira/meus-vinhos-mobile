03e05390140f757819b53c47a82fb4e9aba06292
andrebsiqueira <andreb.siqueira@gmail.com>
2026-09-23 20:40:35 -0300

    Add files via upload

--- views/ocr.py
+++ views/ocr.py
@@ -1,16 +1,11 @@
 import streamlit as st
-from google import genai
+import pandas as pd
+
+from banco import conectar
 
 def show_ocr():
 
     st.markdown(
-        "<div class='secao'>🤖 AI Assistant</div>",
+        '<div class="secao" style="font-size: 28px;">🍷 OCR</div>',
         unsafe_allow_html=True
     )
-
-    try:
-        client = genai.Client()
-
-    except Exception as e:
-        st.error(f"Unable to connect to Gemini: {e}")
-        return
