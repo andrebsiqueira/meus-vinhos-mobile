64fd82b6970941af4cc9a7bc28475fc3c29a70b9
andrebsiqueira <andreb.siqueira@gmail.com>
2026-09-23 21:17:55 -0300

    Reorder Google key concatenation in show_ocr function

--- views/ocr.py
+++ views/ocr.py
@@ -7,7 +7,7 @@ from banco import conectar
 
 def show_ocr():
 
-    google_key = st.secrets["G_KEY1"] + st.secrets["G_KEY3"] + st.secrets["G_KEY2"]
+    google_key = st.secrets["G_KEY1"] + st.secrets["G_KEY2"] + st.secrets["G_KEY3"]
 
     st.write(google_key)
 
