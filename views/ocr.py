58931e39c348ea4a689983ae1229fe970a6389bb
andrebsiqueira <andreb.siqueira@gmail.com>
2026-09-23 21:13:49 -0300

    Enhance show_ocr with API key handling
    Added error handling for API key retrieval and displayed the key.

--- views/ocr.py
+++ views/ocr.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+from google import genai
+
 from banco import conectar
 
 def show_ocr():
@@ -9,3 +11,12 @@ def show_ocr():
         '<div class="secao" style="font-size: 28px;">🍷 OCR</div>',
         unsafe_allow_html=True
     )
+
+    try: 
+        google_key = ( st.secrets["G_KEY1"] + st.secrets["G_KEY2"] + st.secrets["G_KEY3"] ) 
+    except Exception as e: 
+        st.error(f"Error reading API Key: {e}") st.stop()
+
+    st.write(google_key)
+
+    
