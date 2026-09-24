2e9e307805013935085d00380df8cbdb2eb09761
andrebsiqueira <andreb.siqueira@gmail.com>
2026-09-23 21:16:02 -0300

    Refactor show_ocr function in ocr.py
    Removed markdown section and error handling for API key.

--- views/ocr.py
+++ views/ocr.py
@@ -7,16 +7,6 @@ from banco import conectar
 
 def show_ocr():
 
-    st.markdown(
-        '<div class="secao" style="font-size: 28px;">🍷 OCR</div>',
-        unsafe_allow_html=True
-    )
-
-    try: 
-        google_key = ( st.secrets["G_KEY1"] + st.secrets["G_KEY2"] + st.secrets["G_KEY3"] ) 
-    except Exception as e: 
-        st.error(f"Error reading API Key: {e}") st.stop()
-
     st.write(st.secrets["G_KEY1"])
 
     
